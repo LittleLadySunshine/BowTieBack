3ba0a67baaa082488d145976cec875cf43c40375
Denise McCoy <Denise.L.McCoy@gmail.com>
2015-05-26 13:32:33 -0600

    handling users in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::Base
-
   protect_from_forgery with: :exception
+
+  def current_user_id
+    request.headers['HTTP_X-BOWTIE_USER_ID']
+  end
+
+  def current_user
+    User.find_by bowtie_id: current_user_id
+  end
+
+
+  private
+
+  def verify_bowtie_user
+    if current_user_id.nil?
+      respond_to do |format|
+        format.json { render json: { status: :error, message: "Not Authorized" }, status: 403 }
+      end
+      return false
+    end
+    
+
+
+
 end
